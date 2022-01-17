0cf5196df63ccd836fed7489598b44c8bc30529e
arifder12 <dermawana696@gmail.com>
2022-01-17 19:27:46 +0700

    First commit

---
+++ public/js/script.js
@@ -0,0 +1,1 @@
+alert ("Hallo, Selamat Datang diwebsite Saya :) ");
---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+    <h1> Halaman About </h1>
+    <h3> <?php echo $nama; ?> </h3>
+    <p>  <?php echo $email; ?> </p>
+    <img src="images/<?php echo $gambar; ?>" alt="<?php echo $nama; ?>" width="200px">
+</body>
+</html>
